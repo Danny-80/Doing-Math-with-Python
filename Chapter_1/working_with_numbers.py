55b01d7865ba8daaa4d3b31aa633cd651c4bcb93
Danny-80 <danijeldj@hotmail.com>
2022-11-25 10:20:35 +0200

    Add basic mathematical operation in Python

---
+++ Chapter_1/working_with_numbers.py
@@ -0,0 +1,15 @@
+print(3*10)
+print(10/4)
+print(3//2)
+
+# negative divisor
+print(3 // -5)
+print(3//2)
+print(-3//2)
+
+# to get a remainder use modulo operator %
+print(9%3)
+print(8 ** (1/3))
+a = 3
+a + 1
+print(a)
